46d48ed92a158693acc62dbb5d7fd9911ba2b8b7
Pichborith Kong <pichborith@gmail.com>
2024-08-07 12:31:16 -0700

    Java Solution and test for Greatest common divisor of strings

---
+++ Java/src/main/java/TwoPointers/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,17 @@
+package TwoPointers;
+
+public class GreatestCommonDivisorOfStrings {
+
+    public String solution(String str1, String str2) {
+        // Check if concatenated strings are equal or not, if not return ""
+        if (!(str1 + str2).equals(str2 + str1)) return "";
+
+        // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+        int gcd = greatestDivisor(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+
+    private int greatestDivisor(int a, int b) {
+        return b == 0 ? a : greatestDivisor(b, a % b);
+    }
+}
---
+++ Java/src/test/java/TwoPointers/GreatestCommonDivisorOfStringsTest.java
@@ -0,0 +1,50 @@
+package TwoPointers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreatestCommonDivisorOfStringsTest {
+
+    private final GreatestCommonDivisorOfStrings greatestCommonDivisorOfStrings = new GreatestCommonDivisorOfStrings();
+
+    @Test
+    public void case1() {
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+        String expectedResult = "ABC";
+        String actualResult = greatestCommonDivisorOfStrings.solution(str1,
+                                                                      str2);
+        assertEquals(expectedResult, actualResult, "Expected: " + expectedResult + ", Actual Output: "+ actualResult);
+    }
+
+    @Test
+    public void case2() {
+        String str1 = "ABABAB";
+        String str2 = "AB";
+        String expectedResult = "AB";
+        String actualResult = greatestCommonDivisorOfStrings.solution(str1,
+                                                                      str2);
+        assertEquals(expectedResult, actualResult, "Expected: " + expectedResult + ", Actual Output: "+ actualResult);
+    }
+
+    @Test
+    public void case3() {
+        String str1 = "LEET";
+        String str2 = "CODE";
+        String expectedResult = "";
+        String actualResult = greatestCommonDivisorOfStrings.solution(str1,
+                                                                      str2);
+        assertEquals(expectedResult, actualResult, "Expected: " + expectedResult + ", Actual Output: "+ actualResult);
+    }
+
+    @Test
+    public void case4() {
+        String str1 = "ABCDEF";
+        String str2 = "ABC";
+        String expectedResult = "";
+        String actualResult = greatestCommonDivisorOfStrings.solution(str1,
+                                                                      str2);
+        assertEquals(expectedResult, actualResult, "Expected: " + expectedResult + ", Actual Output: "+ actualResult);
+    }
+}

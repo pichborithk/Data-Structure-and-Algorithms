01e100eb4cf1e108c71d6fee6083cc97262eec8e
Pichborith Kong <pichborith@gmail.com>
2024-08-07 10:55:45 -0700

    update Java test for Contain Duplicate

--- Java/src/test/java/ArrayAndHashing/ContainsDuplicateTest.java
+++ Java/src/test/java/ArrayAndHashing/ContainsDuplicateTest.java
@@ -7,9 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContainsDuplicateTest {
 
+    private final ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+
     @Test
     public void case1() {
-        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
-        assertTrue(containsDuplicate.solution(new int[]{1, 2, 3, 1}));
+        int[] nums = {1, 2, 3, 1};
+        boolean actualResult = containsDuplicate.solution(nums);
+        assertTrue(actualResult);
     }
 }

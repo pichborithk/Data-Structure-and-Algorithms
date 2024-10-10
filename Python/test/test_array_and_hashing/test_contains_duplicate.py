32aee70c3ff61cd533e0040d4ba08754943f4f68
Pichborith Kong <pichborith@gmail.com>
2024-10-10 12:32:47 -0700

    rename test case

--- Python/test/test_array_and_hashing/test_contains_duplicate.py
+++ Python/test/test_array_and_hashing/test_contains_duplicate.py
@@ -1,13 +1,13 @@
 from array_and_hashing.contains_duplicate import solution
 
 
-def test_case_1():
+def test_case_one():
     assert solution([1, 2, 3, 1]) == True
 
 
-def test_case_2():
+def test_case_two():
     assert solution([1, 2, 3, 4]) == False
 
 
-def test_case_3():
+def test_case_three():
     assert solution([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
--- Python/test/test_array_and_hashing/test_two_sum.py
+++ Python/test/test_array_and_hashing/test_two_sum.py
@@ -1,13 +1,13 @@
 from array_and_hashing.two_sum import solution
 
 
-def test_case_1():
+def test_case_one():
     assert solution([2, 7, 11, 15], 9) == [0, 1]
 
 
-def test_case_2():
+def test_case_two():
     assert solution([3, 2, 4], 6) == [1, 2]
 
 
-def test_case_3():
+def test_case_three():
     assert solution([3, 3], 6) == [0, 1]
